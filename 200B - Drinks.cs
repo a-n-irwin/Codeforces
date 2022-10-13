befecb0425e2e6aded733e17b60a00fcdc2b6be7
A-N-R-I <100511277+A-N-R-I@users.noreply.github.com>
2022-10-13 13:14:32 +0100

    Create 200B - Drinks.cs

---
+++ 200B - Drinks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace CodeforcesSolutions
+{
+    internal class DrinksSolution
+    {
+        public static void Main()
+        {
+            double pureTotal = 0;
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string[] percent = Console.ReadLine().Split();
+
+            for (int i = 0; i < n; ++i)
+                pureTotal += Convert.ToDouble(percent[i]);
+
+            Console.WriteLine(pureTotal / n);
+        }
+    }
+}
