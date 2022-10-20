3608e9680de348d16b98c2dcaec6c9011d417cd5
A-N-R-I <100511277+A-N-R-I@users.noreply.github.com>
2022-10-20 09:13:44 +0100

    Create 1435A - Finding Sasuke.cs

---
+++ 1435A - Finding Sasuke.cs
@@ -0,0 +1,21 @@
+// 177090918  Oct. 20 09:57:34  Accepted -> 61ms 6420kb
+using System.Text;
+
+int T = int.Parse(Console.ReadLine());
+StringBuilder[] b = new StringBuilder[T];
+
+for (int i = 0; i < T; ++i)
+{
+    int n = int.Parse(Console.ReadLine());
+    
+    string line = Console.ReadLine();
+    string[] input = line.Split();
+    
+    b[i] = new StringBuilder(line.Length);
+    
+    for (int j = 1; j < input.Length; j += 2)
+    {
+        b[i].Append($"{-int.Parse(input[j])} {int.Parse(input[j-1])} ");
+    }
+}
+foreach (var s in b) Console.WriteLine(s);
